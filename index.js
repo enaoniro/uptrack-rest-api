1c94e041670c41ed265222607ae89f9b001f7c40
Ismail Can <caniscanmail@gmail.com>
2023-11-07 15:46:00 +0100

    d11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import "./src/commons/sequelize.js";
 import userRoute from "./src/routes/UserRoute.js";
 import studentRoute from "./src/routes/studentRoute.js";
 // import adminRoute from './src/routes/AdminRoute.js';
+import 'dotenv/config';
 import cantonRoute from "./src/routes/cantonRoute.js";
 import groupRoute from "./src/routes/groupRoute.js";
 import taskRoute from "./src/routes/taskRoute.js";
@@ -13,7 +14,7 @@ import recordRoute from './src/routes/recordRoute.js';
 // import mongoose from "mongoose";
 
 const app = express();
-const port = 3001;
+// const port = 3001;
 // mongoose.connect("mongodb+srv://can:50473524.Su@tasks.b85azwo.mongodb.net/?retryWrites=true&w=majority");
 
 // cors
@@ -34,6 +35,6 @@ app.use("/api/v1/cantons", cantonRoute);
 app.use("/api/v1/groups", groupRoute);
 app.use("/api/v1/students", studentRoute);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
